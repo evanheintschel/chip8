b3a641a9dd4ca00b07ae253a089b2b5dc7fbd175
Evan <Guitarmanrox@Evans-MacBook-Pro.local>
2017-06-14 17:12:49 -0500

    Finished most of the opcodes. Created macros for NNN, NN, N, X, and Y.

--- chip8.cpp
+++ chip8.cpp
@@ -1,3 +1,5 @@
+#include "chip8.h"
+
 #define MEM_SIZE 4096  // 4KB
 #define NUM_REG 16
 #define WIDTH 64
@@ -52,98 +54,145 @@ void chip8::emulateCycle() {
     switch (opcode & 0xF000) {
         case 0x0000:
             switch (opcode & 0x0FFF) {
+		// **********************
                 case 0x00E0:  // 00E0: Clears the screen 
                     // Execute opcode
+		    PC += 2;
                     break;
                 case 0x00EE:  // 00EE: Returns from subroutine
-                    return;
+                    PC += 2;  // update program counter??
+		    return;
                     break;
                 default:
-                     printf("Unknown opcode: 0x%X\n", opcode);
+                    printf("Unknown opcode: 0x%X\n", opcode);
 
             }
             break;
         case 0x100:  // 1NNN: Jumps to address NNN
-            goto opcode & 0x0FFF;
+	    stackPush(PC);
+            goto NNN(opcode);
             break;
         case 0x2000:  // 2NNN: Calls subroutine at NNN
-            *(opcode & 0x0FFF)();
+	    stackPush(PC);
+	    PC = NNN(opcode);
             break;
         case 0x3000:  // 3XNN: Skips the next instruction if VX equals NN
-            if (V[(opcode & 0x0F00) >> 8] == opcode & 0x00FF)
-                // skip
+            if (V[X(opcode)] == NN(opcode))
+                PC += 4;
             break;
         case 0x4000:  // 4XNN: Skips the next instruction if VX does not equal NN
-            if (V[(opcode & 0x0F00) >> 8] != opcode & 0x00FF)
-                // skip
+            if (V[X(opcode)] != NN(opcode))
+                PC += 4;
             break;
         case 0x5000:  // 5XY0: Skips the next instruction if VX equals VY
-            if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])
-                // skip
+            if (V[X(opcode)] == V[Y(opcode)])
+                PC += 4;
             break;
         case 0x6000:  // 6XNN: Sets VX to NN
-            V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+            V[X(opcode)] = NN(opcode);
+	    PC += 2;
             break;
         case 0x7000:  // 7XNN: Adds NN to VX
-            V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
-            break;
+            V[X(opcode)] += NN(opcode);
+            PC += 2;
+	    break;
         case 0x8000:
             switch (opcode & 0x000F) {
                 case 0x0000:  // 8XY0: Sets VX to the value of VY
+		    V[X(opcode)] = V[Y(opcode)];
+		    PC += 2;
                     break;
                 case 0x0001:  // 8XY1: Sets VX to VX OR VY
-                    break;
+		    V[X(opcode)] =V[X(opcode)] | V[Y(opcode)];
+                    PC += 2;
+		    break;
                 case 0x0002:  // 8XY2: Sets VX to VX AND VY
-                    break;
+		    V[X(opcode)] = V[X(opcode)] & V[Y(opcode)];
+                    PC += 2;
+		    break;
                 case 0x0003:  // 8XY3: Sets VX to VX XOR VY
-                    break;
+		    V[X(opcode)] = V[X(opcode)] ^ V[Y(opcode)];
+                    PC += 2;
+		    break;
                 case 0x0004:  // 8XY4: Adds VY to VX
                               // Sets VF to 01 if a carry occurs
                               // Sets VF to 00 if a carry does not occur
-                    break;
+		    if (V[Y(opcode)] > 0xFF - V[X(opcode)])
+			V[0xF] = 1;
+		    else
+			V[0xF] = 0;
+		    V[X(opcode)] += V[Y(opcode)];
+                    PC += 2;
+		    break;
                 case 0x0005:  // 8XY5: Subtracts VY from VX
                               // Sets VF to 01 if a borrow occurs
                               // Sets VF to 00 if a borrow does not occur
- 
+		    if (V[Y(opcode)] > V[X(opcode)])
+			V[0xF] = 1;
+		    else
+			V[0xF] = 0;
+		    V[X(opcode)] -= V[Y(opcode)];
+		    PC += 2;
                     break;
                 case 0x0006:  // 8XY6: Sets VX to VY shifted right by one
                               // Sets VF to least significant bit of VX prior to shift
+		    V[0xF] = V[X(opcode)] & 1;
+		    V[X(opcode)] = V[Y(opcode)] >> 1;
+		    PC += 2;
                     break;
                 case 0x0007:  // 8XY7: Sets VX to VY minus VX
                               // Sets VF to 01 if a borrow occurs
                               // Sets VF to 00 if a borrow does not occur
-
+		    if (V[X(opcode)] > V[Y(opcode)])
+			V[0xF] = 1;
+		    else
+			V[0xF] = 0;
+		    V[X(opcode)] = V[Y(opcode)] - V[X(opcode)];
+		    PC += 2;
                     break;
                 case 0x000E:  // 8XYE: Sets VX to VY shifted left by one
-                              // Sets VF to the most significant bit of VX prior to shift
+                              // Sets VF to the most significant bit of VX (or VY?) prior to shift
+		    V[0xF] = (V[X(opcode)] & 0x8000) >> 15;
+		    V[X(opcode)] = V[Y(opcode)] << 1;
+		    PC += 2;
                     break;
                 default:
                     printf("Unknown opcode: 0x%X\n", opcode);
             }
             break;
         case 0x9000:  // 9XY0: Skips the next instruction if VX does not equal VY
-            if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])
-                // skip
+            if (V[X(opcode)] != V[Y(opcode)])
+                PC += 4;
             break;
         case 0xA000:  // ANNN: Sets I to the address NNN
-            I = opcode & 0x0FFF;
+            I = NNN(opcode);
             PC += 2;
             break;
         case 0xB000:  // BNNN: Jumps to the address NNN plus V0
-            goto opcode & 0x0FFF + V[0x0];
+	    stackPush(pc);
+            goto NNN(opcode) + V[0x0];
             break;
         case 0xC000:  // CXNN: Sets VX to the result of a bitwise AND operation on a random number [0, 255] and NN
-            V[(opcode & 0x0F00) >> 8] = (rand() % 256) & (opcode & 0x00FF);
+            V[X(opcode)] = (rand() % 256) & NN(opcode);
+	    PC += 2;
             break;
+	// ******************************
         case 0xD000:  // DXYN: Draws a sprite at (VX, VY) with width of 8 pixels and height of N pixels starting at address stored in I
                       // Sets VF to 01 if any set pixels are changed to unset, and 00 otherwise
+	    PC += 2;
             break;
         case 0xE000:
             switch (opcode & 0x00FF) {
+		// ************************
                 case 0x009E:  // EX9E: Skips the next instruction if the key stored in VX is pressed
+		    //if (pressed)
+		//	PC += 4;
                     break;
+		// ************************
                 case 0x00A1:  // EXA1: Skips the next instruction if the key stored in VX is not pressed
-                    break;
+                    //if (not pressed)
+		//	PC += 4;
+		    break;
                 default:
                     printf("Unknown opcode: 0x%X\n", opcode);
             }
@@ -151,25 +200,48 @@ void chip8::emulateCycle() {
         case 0xF000:
             switch (opcode & 0x00FF) {
                 case 0x0007:  // FX07: Sets VX to the value of the delay timer
+		    V[X(opcode)] = delay_timer;
+		    PC += 2;
                     break;
+		// ***********************
                 case 0x000A:  // FX0A: Waits for a keypress and stores the result in VX
-                    break;
+                    PC += 2;
+		    break;
                 case 0x0015:  // FX15: Sets the delay timer to VX
-                    break;
+		    delay_timer = V[X(opcode)];
+                    PC += 2;
+		    break;
                 case 0x0018:  // FX18: Sets the sound timer to VX
-                    break;
+		    sound_timer = V[X(opcode)];
+                    PC += 2;
+		    break;
                 case 0x001E:  // FX1E: Adds VX to I
-                    break;
+		    I += V[X(opcode)];
+                    PC += 2;
+		    break;
                 case 0x0029:  // FX29: Sets I to the location of the sprite corresponding to the value stored in VX
-                    break;
+                    PC += 2;
+		    break;
                 case 0x0033:  // FX33: Stores the binary-coded decimal equivalent of VX at address I, I + 1, I + 2
-                    break;
+                    memory[I] = V[X(opcode)] / 100;
+		    memory[I + 1] = (V[X(opcode)] / 10) % 10;
+		    memory[I + 2] = (V[X(opcode)] % 100) % 10;
+		    PC += 2;
+		    break;
                 case 0x0055:  // FX55: Stores the values of registers [V0, VX] in memory starting at address I
                               // Sets I to I plus X plus 1 after operation
-                    break;
+		    for (unsigned short i = 0; i <= X(opcode); i++)
+			memory[I + i] = V[i];
+		    I += X(opcode) + 1;
+                    PC += 2;
+		    break;
                 case 0x0066:  // FX66: Fills registers [V0, VX] with values stored in memory starting at address I
                               // Sets I to I plus X plus 1 after operation
-                    break;
+		    for (unsigned short i = 0; i <= X(opcode); i++)
+			V[i] = memory[I + i];
+		    I += X(opcode) + 1;
+                    PC += 2;
+		    break;
                 default:
                     printf("Unknown opcode: 0x%X\n", opcode);
 
@@ -192,4 +264,16 @@ void chip8::emulateCycle() {
 
 void chip8::setKeys() {
     
+}
+
+void chip8::stackPush(unsigned short val) {
+    ASSERT (sp < STACK_SIZE);
+    stack[sp] = val;
+    sp++;
+}
+
+unsigned short chip8::stackPop() {
+    ASSERT (sp > 0);
+    sp--;
+    return stack[sp];
 }
---
+++ chip8.h
@@ -0,0 +1,5 @@
+#define NNN(x) x & 0x0FFF
+#define NN(x) x & 0x00FF
+#define N(x) x & 0x000F
+#define X(x) (x & 0x0F00) >> 8
+#define Y(x) (x & 0x00F0) >> 4
